558c78d4afdb5d00f02bee80c700f56a89c89ffd
jahid hasan <hjahid1411@gmail.com>
2022-02-15 20:25:37 +0300

    ff

--- loader.js
+++ loader.js
@@ -3,7 +3,7 @@ $(window).on('load', function(){
   setTimeout(removeLoader, 10000); //wait for page load PLUS two seconds.
 });
 function removeLoader(){
-    $( "#loader" ).fadeOut(5000, function() {
+    $( "#loader" ).fadeOut(2000, function() {
       // fadeOut complete. Remove the loading div
       $( "#loader" ).remove(); //makes page more lightweight 
   });  
