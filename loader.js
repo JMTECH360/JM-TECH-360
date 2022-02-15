47995a0455f9dd8b23106e8ae873149073e709fe
jahid hasan <hjahid1411@gmail.com>
2022-02-15 20:22:19 +0300

    page loader added

---
+++ loader.js
@@ -0,0 +1,10 @@
+$('body').append('<div style="" id="loader"><div></div></div>');
+$(window).on('load', function(){
+  setTimeout(removeLoader, 10000); //wait for page load PLUS two seconds.
+});
+function removeLoader(){
+    $( "#loader" ).fadeOut(5000, function() {
+      // fadeOut complete. Remove the loading div
+      $( "#loader" ).remove(); //makes page more lightweight 
+  });  
+}
